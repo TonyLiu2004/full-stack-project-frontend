c6e539fd9debae3d56fc8bc6d928f2b9a0bc576a
Tonyliu2004 <ltsplice@gmail.com>
2024-04-09 20:46:04 -0400

    Initial

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -4,14 +4,15 @@
 It contains all Thunk Creators and Thunks.
 ================================================== */
 import * as ac from './actions/actionCreators';  // Import Action Creators ("ac" keyword Action Creator)
-const axios = require('axios');
+import axios from 'axios';
+//const axios = require('axios');
 
 //All Campuses
 // THUNK CREATOR:
 export const fetchAllCampusesThunk = () => async (dispatch) => {  // The THUNK
   try {
     // API "get" call to get "campuses" data from database
-    let res = await axios.get(`/api/campuses`);  
+    let res = await axios.get(`http://localhost:5001/api/campuses`);  
     // Call Action Creator to return Action object (type + payload with "campuses" data)
     // Then dispatch the Action object to Reducer to update state 
     dispatch(ac.fetchAllCampuses(res.data));
@@ -25,7 +26,7 @@ export const fetchAllCampusesThunk = () => async (dispatch) => {  // The THUNK
 export const fetchCampusThunk = (id) => async (dispatch) => {  // The THUNK
   try {
     // API "get" call to get a student data (based on "id")from database
-    let res = await axios.get(`/api/campuses/${id}`);  
+    let res = await axios.get(`http://localhost:5001/api/campuses/${id}`);  
     dispatch(ac.fetchCampus(res.data));
   } catch(err) {
     console.error(err);
@@ -37,7 +38,7 @@ export const fetchCampusThunk = (id) => async (dispatch) => {  // The THUNK
 export const fetchAllStudentsThunk = () => async (dispatch) => {  // The THUNK
   try {
     // API "get" call to get "students" data from database
-    let res = await axios.get(`/api/students`);  
+    let res = await axios.get(`http://localhost:5001/api/students`);  
     // Call Action Creator to return Action object (type + payload with "students" data)
     // Then dispatch the Action object to Reducer to update state 
     dispatch(ac.fetchAllStudents(res.data));  
@@ -51,7 +52,7 @@ export const fetchAllStudentsThunk = () => async (dispatch) => {  // The THUNK
 export const addStudentThunk = (student) => async (dispatch) => {  // The THUNK
   try {
     // API "post" call to add "student" object's data to database
-    let res = await axios.post(`/api/students`, student);  
+    let res = await axios.post(`http://localhost:5001/api/students`, student);  
     // Call Action Creator to return Action object (type + payload with new students data)
     // Then dispatch the Action object to Reducer to update state 
     dispatch(ac.addStudent(res.data));
@@ -66,7 +67,7 @@ export const addStudentThunk = (student) => async (dispatch) => {  // The THUNK
 export const deleteStudentThunk = studentId => async dispatch => {  // The THUNK
   try {
     // API "delete" call to delete student (based on "studentID") from database
-    await axios.delete(`/api/students/${studentId}`);  
+    await axios.delete(`http://localhost:5001/api/students/${studentId}`);  
     // Delete successful so change state with dispatch
     dispatch(ac.deleteStudent(studentId));
   } catch(err) {
@@ -79,7 +80,7 @@ export const deleteStudentThunk = studentId => async dispatch => {  // The THUNK
 export const editStudentThunk = student => async dispatch => {  // The THUNK
   try {
     // API "put" call to update student (based on "id" and "student" object's data) from database
-    let updatedStudent = await axios.put(`/api/students/${student.id}`, student); 
+    let updatedStudent = await axios.put(`http://localhost:5001/api/students/${student.id}`, student); 
     // Update successful so change state with dispatch
     dispatch(ac.editStudent(updatedStudent));
   } catch(err) {
@@ -92,7 +93,7 @@ export const editStudentThunk = student => async dispatch => {  // The THUNK
 export const fetchStudentThunk = id => async dispatch => {  // The THUNK
   try {
     // API "get" call to get a specific student (based on "id") data from database
-    let res = await axios.get(`/api/students/${id}`);  
+    let res = await axios.get(`http://localhost:5001/api/students/${id}`);  
     // Call Action Creator to return Action object (type + payload with student data)
     // Then dispatch the Action object to Reducer to display student data 
     dispatch(ac.fetchStudent(res.data));
